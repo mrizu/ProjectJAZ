35607c5329ac20b839eb06b94251d1575f54866c
karol <karol0704@wp.pl>
2022-01-07 19:08:58 +0100

    init

---
+++ src/main/java/com/example/mrizudev/democracyinfo/DemokracjaInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mrizudev.democracyinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokracjaInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemokracjaInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mrizudev/democracyinfo/DemokracjaInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mrizudev.democracyinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemokracjaInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
